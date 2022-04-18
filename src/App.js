24af69cbf4195f63e7f14b52456aab307fca2cc0
arif <mirzaarifali3@gmail.com>
2022-04-18 22:59:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.rtl.min.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>E-comm Project</h1>
+      <button className='btn btn-primary'>click me</button>
+    </div>
+  );
+}
+
+export default App;
